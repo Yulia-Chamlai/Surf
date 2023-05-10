3d2874cf0d57fa441c011028e6821f45610d4cd5
Yulia-Chamlai <yuliachamlai@gmail.com>
2023-05-10 14:20:00 +0300

    second section

--- js/script.js
+++ js/script.js
@@ -14,4 +14,21 @@ $(function () {
     slidesToScroll: 4,
     asNavFor: ".header__slider",
   });
+  $(".surf-slider").slick({
+    slidesToShow: 8,
+    slidesToScroll: 8,
+    asNavFor: ".image-slider",
+    arrows: false,
+    focusOnSelect: true,
+  });
+  $(".image-slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    prevArrow:
+      '<button class="prev-btn" type="button"><img src="./images/prev-arrow.svg" alt="Icon"></button>',
+    nextArrow:
+      '<button class="next-btn" type="button"><img src="./images/next-arrow.svg" alt="Icon"></button>',
+    asNavFor: ".surf-slider",
+  });
 });
